25affd7a65e89cd457b9abf555c0f86b811e81fb
Varsha Jagadeesh <varshajagadeesh.23@gmail.com>
2026-04-10 10:15:24 -0400

    Initial Commit

---
+++ Dog_Breeds_App/Dog_Breeds_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dog_Breeds_AppApp.swift
+//  Dog_Breeds_App
+//
+//  Created by Varsha Jagadeesh on 4/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Dog_Breeds_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
